b5dcdd7d46eec5e387143dece71a244930a5cf04
johneppillar <johneppillar@gmail.com>
2017-04-12 18:24:21 +0100

    started work on news skill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 'use strict';
-var Alexa  = require('alexa-sdk'),
-    APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+const Alexa  = require('alexa-sdk'),
+      rp     = require('request-promise'),
+      dotenv = require('dotenv'),
+      Speech = require('ssml-builder');
 
+// Load env vars
+dotenv.load()
+
+var APP_ID = process.env.appid;
+
+//=========================================================
+// Helper functions
+//=========================================================
+
+// Call a remote API to get data
+function requestAPIdata (apiURL, userAgent) {
+    var options = {
+        'User-Agent': userAgent,
+        method: 'GET',
+        uri: apiURL,
+        resolveWithFullResponse: true,
+        json: true
+    }
+    return rp(options);
+};
+
+// Alexa skill
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     //alexa.APP_ID = APP_ID;
@@ -9,19 +33,36 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
+// Intents
 var handlers = {
 //    'LaunchRequest': function () {
 //        this.emit('HelloIntent');
 //    },
     'HelloIntent': function () {
-//TODO
-
         this.emit(':tell', "hello there.");
-
-
-
-
     },
+
+
+    'NewsIntent': function () {
+        var url = 'http://localhost:3978/joke';
+        requestAPIdata(url)
+        .then(function(apiData){
+            // Get the joke data
+            apiData = apiData.body;
+            // Send response back to alexa
+            this.emit(':tell', apiData.data);
+            //sendResponse(apiData.joke);
+        })
+        .catch(function(err){
+            // Send response back to caller
+            //alfredHelper.sendResponse(res, 'error', err.message);
+            //logger.error('joke: ' + err);
+        });
+    },
+
+
+
+
     'AMAZON.HelpIntent': function () {
         this.emit(':tell', "hello there, you asked for help.");
 //        var speechOutput = "";
